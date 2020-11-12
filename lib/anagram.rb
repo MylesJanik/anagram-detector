b873b8d04b036ca0808c67d32cb421121ad06d2c
Myles <mylesjanik@gmail.com>
2020-11-11 16:11:08 -0800

    commit tree 42578ba7f29d207036066f064da4dfd27a5ebecf

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word.chars.sort(&:casecmp).join
+    end
+
+    def match(array)
+        @new_array = []
+        array.each do |a|
+            if a.chars.sort(&:casecmp).join == @word
+                @new_array << a
+            end
+        end
+        return @new_array
+    end
+end
